2a592ec64e97474035e18fb416a3626332ecacd5
Lucas Ribeiro dos Santos <lucas1995ls@outlook.com>
2022-05-17 19:23:39 -0300

    test dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ export function App() {
 
   return (
     <div >
+       <h1 className="text-xl leading-6 " >FEEDBACK</h1>
+       <h2 >Clique no Widget abaixo para dar seu feedback!</h2>
+  
       <Widget />
       
     </div>
