646d050adfde81e46e7ac6890f1282bcf58703e0
Lucas Ribeiro dos Santos <lucas1995ls@outlook.com>
2022-05-18 09:53:00 -0300

    index-

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ export function App() {
 
   return (
     <div >
-       <h1 className="text-xl leading-6 " >FEEDBACK</h1>
-       <h2 >Clique no Widget abaixo para dar seu feedback!</h2>
-  
+      <h2 className="flex justify-center ">
+        Clique no Widget abaixo para dar seu feedback!
+      </h2>
+
       <Widget />
       
     </div>
